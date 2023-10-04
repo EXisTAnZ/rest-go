125e7a65d3a913f6169c1267ba2d3e929946d28e
EXisTAnZ <ozim_ssspam@mail.ru>
2023-10-04 11:47:47 +0300

    feat: add products endpoint

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Server started at 5000 port")
+	connectionString := "localhost:5000"
+	router := gin.Default()
+	router.GET("/products", getProducts)
+	router.Run(connectionString)
+	fmt.Println("Server started at " + connectionString + " port")
+}
+
+type product struct {
+	Id          string  `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Count       int     `json:"count"`
+}
+
+var products = []product{
+	{Id: "1", Title: "Очарованье осени", Description: "Данный шедевр навеян маминым борщом", Price: 1500.00, Count: 3},
+	{Id: "2", Title: "Нежный шепот", Description: "Модному явлению asmr посвящается", Price: 2500.00, Count: 2},
+	{Id: "3", Title: "Хронический синусит", Description: "Этот аромат пробудит спящие рецепторы в вашем носу", Price: 1300.00, Count: 1},
+}
+
+func getProducts(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, products)
 }
